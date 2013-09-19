4a1d1af98d705c19fd4d3be7902ecb2e36f546d8
emortiz <montenegro.eddy@gmail.com>
2013-09-18 23:25:00 -0700

    commit changes

--- contacbusiness/contac-server/modules/modelo/src/main/java/contac/modelo/eao/productoEAO/ProductoEAO.java
+++ contacbusiness/contac-server/modules/modelo/src/main/java/contac/modelo/eao/productoEAO/ProductoEAO.java
@@ -20,15 +20,32 @@ public interface ProductoEAO extends GenericEAO<Producto, Integer> {
     /**
      * Busca producto con parametros
      *
-     * @param codigo, Codigo del producto
-     * @param nombre, Nombre del producto
+     * @param codigo,           Codigo del producto
+     * @param nombre,           Nombre del producto
      * @param codigoFabricante, Codigo del fabricante
      * @return List<Producto>
-     * @throws PersistenceClassNotFoundException, Exception
-     * @throws GenericPersistenceEAOException, Exception
+     * @throws PersistenceClassNotFoundException,
+     *          Exception
+     * @throws GenericPersistenceEAOException,
+     *          Exception
      */
     public List<Producto> find(String codigo, String nombre, String codigoFabricante) throws PersistenceClassNotFoundException,
             GenericPersistenceEAOException;
+
+    /**
+     * Buscar productos con parametros
+     *
+     * @param codigoDesde,  Codigo Desde
+     * @param codigoHasta,  Codigo Hasta
+     * @param idLinea,      Linea de Producto
+     * @param idAlmacen,    Codigo de Almacen
+     * @param codProveedor, Codigo Proveedor
+     * @return List
+     * @throws GenericPersistenceEAOException,
+     *          Exception
+     */
+    public List<Producto> find(String codigoDesde, String codigoHasta, Integer idLinea, Integer idAlmacen,
+                               Long codProveedor) throws GenericPersistenceEAOException;
 
     /**
      * Busca producto por su codigo
